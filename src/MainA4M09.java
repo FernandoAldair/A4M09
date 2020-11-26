ebf1b5336c8e6d3e01473eb9bb9f9814684d654c
Fernando <fcano@elpuig.xeill.net>
2020-11-26 19:33:21 +0100

    Initial commit

---
+++ src/MainA4M09.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class MainA4M09 {
+    public static void main(String[] args) {
+        Xifrar xifrar = new Xifrar();
+
+        Path ruta = Paths.get("/home/dam2a/Escriptori/A4/textamagat");
+
+
+        
+
+    }
+}
